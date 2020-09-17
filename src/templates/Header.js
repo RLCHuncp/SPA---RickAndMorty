161452c7c250f1703e6d678c3e6cc53efd2e5e87
RLCHuncp <rlch06.uncp@gmail.com>
2020-09-17 10:55:33 -0500

    Creando templade de Header

---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const view = `
+    <div class="Header-main">
+        <div className="Header-logo">
+            <h1>
+                <a href="/">
+                    100tifi.co
+                </a>
+            </h1>
+        </div>
+        <div className="Header-nav">
+            <a href="#/about">
+                About
+            </a>
+        </div>
+    </div>
+    `;
+    return view;
+};
+
+export default Header;
