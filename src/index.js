bd5c3b719d20bebbbc2071cbbb6ab78c99be77d8
RLCHuncp (via Travis CI) <rlch06.uncp@gmail.com>
2020-09-18 01:54:42 +0000

    Deploy Release

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import router from './routes';
-
-// Cuando carga por primera vez
-window.addEventListener('load', router);
-//Encargado de escuchar a las rutas, cada vez que hay cambios
-window.addEventListener('hashchange', router);
