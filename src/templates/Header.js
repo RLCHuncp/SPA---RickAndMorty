42e214e295450fc019b8a2d63cc4fe7db25d26f0
RLCHuncp <rlch06.uncp@gmail.com>
2020-09-17 21:42:56 -0500

    Header ruta

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
         <div class="Header-logo">
             <h1>
-                <a href="/#">
+                <a href="/SPA---RickAndMorty/#/">
                 <i class="fas fa-home"></i>
                 RickAndMorty
                 </a>
